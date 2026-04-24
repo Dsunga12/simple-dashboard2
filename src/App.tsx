aeb7285ef8415f217755f5163a11332a4925bead
Dsunga12 <danielsunga03@gmail.com>
2026-04-24 18:54:04 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 export default function App() {
   return (
-    <div style={{padding:40,color:"white",background:"#0f172a",minHeight:"100vh"}}>
-      <h1>🔥 Dashboard Working!</h1>
-      <p>Vercel deployment successful.</p>
+    <div style={{ height: "100vh", margin: 0 }}>
+      
+      <h1 style={{ padding: "10px", color: "white", background: "#0f172a" }}>
+        🚀 My AI Dashboard
+      </h1>
+
+      <iframe
+        src="https://ai.studio/apps/ee0b21de-b9f1-41e2-a6d4-e2be73c485a7"
+        style={{
+          width: "100%",
+          height: "90%",
+          border: "none"
+        }}
+      />
+
     </div>
   );
 }
